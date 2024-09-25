754cb41c4664f27a05d983a7bcd98701f4683ac7
emanuelli <emanuelli.olievira@etec.sp.gov.br>
2024-09-25 11:21:24 +0000

    [feat] async storage users

--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -1,5 +1,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useUserDatabase } from "../../database/useUserDatabase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { Text } from "react-native";
 
 const AuthContext = createContext({});
 
@@ -17,6 +20,39 @@ export function AuthProvider({ children }) {
   });
   const { authUser } = useUserDatabase();
 
+  useEffect(() => {
+    const loadStoragedData = async () => {
+      const storagedUser = await AsyncStorage.getItem("@payment:user");
+      if (storagedUser) {
+        setUser({
+          autenticated: true,
+          user: JSON.parse(storagedUser),
+          role: JSON.parse(storagedUser),
+        });
+      } else {
+        setUser({
+          autenticated: false,
+          user: null,
+          role: null,
+        });
+      }
+    };
+    loadStoragedData();
+  }, []);
+  useEffect(() => {
+    console.log("AuthProvider: ", user)
+}, [user]);
+
+if (user?.autenticated === null) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 28, marginTop: 15 }}>
+        Carregando dados do usuário
+      </Text>
+      <ActivityIndicator size='large' color='#0000ff'/>
+    </View>
+  );
+}
   const signIn = async ({ email, password }) => {
     const response = await authUser({ email, password });
     console.log(response);
@@ -30,7 +66,7 @@ export function AuthProvider({ children }) {
       throw new Error("Usuário ou senha inválidos");
     }
    
-
+    await AsyncStorage.deleteItem("@payment:user", JSON.stringify(response));
     setUser({
       autenticated: true,
       user: responsive,
@@ -38,6 +74,7 @@ export function AuthProvider({ children }) {
     });
   };
   const signOut = async () => {
+    await AsyncStorage.deleteItem("@payment:user");
     setUser({});
   };
   useEffect(() => {
